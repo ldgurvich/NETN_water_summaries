dc3bbde96a6378b71b164656255620dc4c6d04bd
ldgurvich <ldgurvich@gmail.com>
2020-10-05 11:19:51 -0400

    added label variables

--- scripts/ggplot_band_example.R
+++ scripts/ggplot_band_example.R
@@ -1,10 +1,10 @@
+
+devtools::install_github("NCRN/NCRNWater", ref = "NETNsummaries")
+
 #----- Load libraries -----
 library(NCRNWater)
 library(tidyverse)
-library(matlib)
 library(plotly)
-library(listviewer) # to be able to display plotly JSON object
-library(scales)
 
 #----- Import the data -----
 path = "C:/Users/Diana/Documents/NETN/Water/data" #change to your path
@@ -17,30 +17,37 @@ netnwd <- importNCRNWater(Dir = path,
 # NOTE: This is set up for lower NETN only, and assumes there's not April data.
 # For ACAD sites, need to update for April, including the x_axis_pad and scale_x_continuous
 getSiteInfo(netnwd, park = "MABI", info = "SiteCode")
-site = "NETN_MABI_PA00"
+site = "NETN_MABI_SA00"
 park = substr(site, 6, 9)
 sitename = getSiteInfo(netnwd, parkcode = park, sitecode = site, info = "SiteName")
 
 char_list <- getCharInfo(netnwd, park = park, sitecode = site, 
                         category = "physical", info = "CharName")
-char <- char_list[1]
+char <- char_list[7]
 
 unit <- getCharInfo(netnwd, park = park, sitecode = site, charname = char, info = "Units") %>% 
-  ifelse(. == "percent", paste("%"), .) %>% 
+  ifelse(. == "pct", paste("%"), .) %>% 
   ifelse(. == "pH units", paste(""), .)
 
+# Find year range of data
+#water_dat_hist <- getWData(netnwd, park = park, sitecode = site, charname = char)
+
+#min <- min(lubridate::year(water_dat_hist$Date), na.rm = T)
+#max <- max(lubridate::year(water_dat_hist$Date), na.rm = T)
+
+# Compile historic water data
 water_dat_hist <- getWData(netnwd, park = park, sitecode = site, 
                            charname = char, years = 2006:2018) %>% 
   mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE)) 
 
+# Compile target year water data
 water_dat_new <- getWData(netnwd, park = park, sitecode = site,
                           charname = char, years = 2019) %>% 
   mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE),
          mon_num = as.numeric(month))
+water_dat_new$year <- format(as.Date(water_dat_new$Date), "%Y")
 
-ylabel = getCharInfo(netnwd, parkcode = park, sitecode = site, charname = char,
-                     info = "DisplayName")
-
+# Create percentile bands for input df
 pct_fun <- function(df){
   # Check if month is a column, and add if it's not.
   df <- if(!"month" %in% names(df)){
@@ -62,9 +69,11 @@ pct_fun <- function(df){
   return(df_sum)
 }
 
+# Historic percentile band values
 water_pct <- pct_fun(water_dat_hist) 
 head(water_pct)
 
+# Smooth bands with monthly midpoints
 loess_bands <- function(df, column, band){
   df2 <- df[ , c(column, "mon_num")]
   colnames(df2) <- c("y", "mon_num")
@@ -75,8 +84,12 @@ loess_bands <- function(df, column, band){
   return(loess_pred)
 }
 
+# Columns with band values + median ([6])
 col_list <- names(water_pct[, 6:12])
+
+# Create list of shortened column names coinciding with col_list
 band_list <- c("median", "l100", "u100", "l95", "u95", "l50", "u50") 
+
 
 loess_map <- map2(col_list, band_list, ~loess_bands(water_pct, .x, .y)) %>% data.frame() %>% 
   mutate(mon_num = c(5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10))
@@ -87,6 +100,15 @@ final_data <- merge(water_dat_new, loess_res, by = c("mon_num", "Park", "month",
                     all.x = T, all.y = T)
 
 str(final_data)
+
+# Create y axis label with units in parentheses, unless it's pH (no units)
+ylabel <- getCharInfo(netnwd, parkcode = park, sitecode = site, charname = char,
+                      info = "DisplayName") %>% 
+          ifelse(. != "pH", paste0(.," (", unit, ")"), .)
+
+# Create label for point data by removing units from char 
+# (this still needs to be improved)
+ptlabel <- gsub("_.*","",char)
 
 monthly_plot <- 
   ggplot(data = final_data, aes(x = mon_num, y = median_val))+
@@ -99,77 +121,17 @@ monthly_plot <-
   stat_smooth(method = "loess", aes(x = mon_num, y = median_val, text = "Historic median"), 
               color = "#1A52D0",
               position = "identity", se = F, formula = y ~ x, span = 0.8)+
-  labs(y = ylabel, x = NULL, title = sitename)+  
-  geom_point(data = water_dat_new, aes(x = mon_num, y = ValueCen,
-                                       text = paste0(month, " ", year, "<br>", 
-                                                     char, ": ", round(ValueCen,1))
-  ))+
+  labs(y = ylabel, x = NULL, title = sitename) +  
+  geom_point(aes(x = mon_num, y = ValueCen, text = paste0(month, " ", year, "<br>", 
+                                                          ptlabel, ": ", round(ValueCen,1), " ", unit))) +
   forestMIDN::theme_FVM()+
   theme(plot.title = element_text(hjust = 0.5))+
   scale_x_continuous(breaks = c(5, 6, 7, 8, 9, 10), 
                      labels = c("5" = "May", "6" = "Jun", "7" = "Jul", "8" = "Aug", 
                                 "9" = "Sep", "10" = "Oct")) #update for ACAD
+monthly_plot
 
-ggplotly(monthly_plot, tooltip = c("text", "Smoothed line", "Band 1", "Band 2", "Band 3"))
+ggplotly(monthly_plot, tooltip = c("text"))
 
-
-
-# #loess_res <- cbind(water_pct, loess_map)
-# loess_res <- merge(water_pct, loess_map, by = "mon_num", all.x = T, all.y = T)
-# view(loess_res)
-# loess_res$x_axis_pad <- c(4.9, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10.1) # remember to update for ACAD
-# 
-# # original working plot
-# monthly_plot <- 
-#   ggplot(data = loess_res, aes(x = mon_num, y = median_val))+
-#   geom_ribbon(aes(x = x_axis_pad, ymax = smooth_u100, ymin = smooth_l100), 
-#               fill = "#89A7E7", alpha = 0.3)+
-#   geom_ribbon(aes(x = x_axis_pad, ymax = smooth_u95, ymin = smooth_l95), 
-#               fill = "#89A7E7", alpha = 0.4)+
-#   geom_ribbon(aes(x = x_axis_pad, ymax = smooth_u50, ymin = smooth_l50), 
-#               fill = "#89A7E7", alpha = 0.8)+
-#   stat_smooth(method = "loess", aes(text = "Median"), 
-#               color = "#1A52D0",
-#               position = "identity", se = F, formula = y ~ x, span = 0.8)+
-#   labs(y = paste0(ylabel," (", unit, ")"), x = NULL, title = sitename)+  
-#   geom_point(data = water_dat_new, aes(x = mon_num, y = ValueCen, 
-#                                        text = paste0(month, ": ", round(ValueCen, 2)," ", unit)
-#                                        )) + 
-#   forestMIDN::theme_FVM()+
-#   theme(plot.title = element_text(hjust = 0.5))+
-#   scale_x_continuous(breaks = c(5, 6, 7, 8, 9, 10), 
-#                      labels = c("5" = "May", "6" = "Jun", "7" = "Jul", "8" = "Aug", 
-#                                 "9" = "Sep", "10" = "Oct")) # remember to update for ACAD
-# 
-# ggplotly(monthly_plot, tooltip = "text")
-# 
-# # View plotly JSON object
-# plotly_json(monthly_plot)
-# 
-# # ------------------------
-# # Merge loess_res and water_dat_new to see if that fixes the duplicates in plotly (it didn't)
-# loess_newdat <- merge(loess_res, water_dat_new, all.x = T, all.y = T)
-# 
-# # new test plot with merged dataframe
-# monthly_plot <-
-#   ggplot(data = loess_newdat, aes(x = mon_num, y = median_val)) +
-#   geom_ribbon(aes(x = x_axis_pad, ymax = smooth_u100, ymin = smooth_l100),
-#               fill = "#89A7E7", alpha = 0.3) +
-#   geom_ribbon(aes(x = x_axis_pad, ymax = smooth_u95, ymin = smooth_l95),
-#               fill = "#89A7E7", alpha = 0.4) +
-#   geom_ribbon(aes(x = x_axis_pad, ymax = smooth_u50, ymin = smooth_l50),
-#               fill = "#89A7E7", alpha = 0.8) +
-#   stat_smooth(method = "loess", color = "#1A52D0", aes(text = ),
-#               position = "identity", se = F, formula = y ~ x, span = 0.8) +
-#   labs(y = ylabel, x = NULL, title = sitename) +
-#   geom_point(aes(x = mon_num, y = ValueCen)) +
-#               forestMIDN::theme_FVM() +
-#   theme(plot.title = element_text(hjust = 0.5)) +
-#   scale_x_continuous(breaks = c(5, 6, 7, 8, 9, 10),
-#                      labels = c("5" = "May", "6" = "Jun", "7" = "Jul", "8" = "Aug",
-#                                 "9" = "Sep", "10" = "Oct")) # remember to update for ACAD
-# 
-# ggplotly(monthly_plot)
-
-
-
+# View plotly JSON object
+#plotly_json(monthly_plot)
