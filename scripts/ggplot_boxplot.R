8cb73dd8efe999cb17dbd2abccf4800bd8e19a28
ldgurvich <ldgurvich@gmail.com>
2020-10-18 16:29:45 -0400

    boxplot draft; all park sites

---
+++ scripts/ggplot_boxplot.R
@@ -0,0 +1,56 @@
+#----- Load libraries -----
+library(NCRNWater)
+library(tidyverse)
+library(plotly)
+
+#----- Import the data -----
+path = "C:/Users/Diana/Documents/NETN/Water/data" # change to your path
+
+netnwd <- importNCRNWater(Dir = path, 
+                          Data = "Water Data.csv", 
+                          MetaData = "VizMetaData.csv")
+
+#----- Set up site and parameter info -----
+#site = "NETN_MABI_SA00"
+park = "MABI"
+site <- getSiteInfo(netnwd, park = park, info = "SiteCode") # get list of sites
+sitename = getSiteInfo(netnwd, parkcode = park, sitecode = site, info = "SiteName")
+
+char_list <- getCharInfo(netnwd, park = park, sitecode = site, 
+                         category = "physical", info = "CharName")
+char <- char_list[1]
+
+unit <- getCharInfo(netnwd, park = park, sitecode = site, charname = char, info = "Units") %>% # fix this, doesn't work with two sites
+  ifelse(. == "pct", paste("%"), .) %>% 
+  ifelse(. == "pH units", paste(""), .)
+
+# Compile historic water data
+water_dat_hist <- getWData(netnwd, park = park, sitecode = site, 
+                           charname = char, years = 2006:2018) %>% 
+                  mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE)) 
+
+# Compile target year water data
+water_dat_new <- getWData(netnwd, park = park, sitecode = site,
+                          charname = char, years = 2019) %>% 
+                 mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE),
+                        mon_num = as.numeric(month))
+water_dat_new$year <- format(as.Date(water_dat_new$Date), "%Y")
+
+#----- Boxplot -----
+site_plot <- 
+  ggplot(data = water_dat_hist, aes(x = site, y = ValueCen) +
+  geom_boxplot()
+  
+
+site_plot
+
+
+
+
+
+
+
+
+
+
+
