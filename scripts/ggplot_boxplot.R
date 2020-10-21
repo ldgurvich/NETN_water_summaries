80acb10fd82691967bd555f3eaf28613f5507ebf
ldgurvich <ldgurvich@gmail.com>
2020-10-21 08:55:12 -0400

    changed path and water_dat compiler

--- scripts/ggplot_boxplot.R
+++ scripts/ggplot_boxplot.R
@@ -4,7 +4,8 @@ library(tidyverse)
 library(plotly)
 
 #----- Import the data -----
-path = "C:/Users/Diana/Documents/NETN/Water/data" # change to your path
+#path = "C:/Users/Diana/Documents/NETN/Water/data" # change to your path
+path = "C:/Users/DianaG/Documents/Work/NETN/Water/data"
 
 netnwd <- importNCRNWater(Dir = path, 
                           Data = "Water Data.csv", 
@@ -24,24 +25,30 @@ unit <- getCharInfo(netnwd, park = park, sitecode = site, charname = char, info 
   ifelse(. == "pct", paste("%"), .) %>% 
   ifelse(. == "pH units", paste(""), .)
 
-# Compile historic water data
-water_dat_hist <- getWData(netnwd, park = park, sitecode = site, 
-                           charname = char, years = 2006:2018) %>% 
-                  mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE)) 
+water_dat <- getWData(netnwd, park = park, sitecode = site,
+                      charname = char, years = 2006:2019) %>% 
+             mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE))
 
-# Compile target year water data
-water_dat_new <- getWData(netnwd, park = park, sitecode = site,
-                          charname = char, years = 2019) %>% 
-                 mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE),
-                        mon_num = as.numeric(month))
-water_dat_new$year <- format(as.Date(water_dat_new$Date), "%Y")
+
+# Compile historic water data
+# water_dat_hist <- getWData(netnwd, park = park, sitecode = site, 
+#                            charname = char, years = 2006:2018) %>% 
+#                   mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE)) 
+
+# # Compile target year water data
+# water_dat_new <- getWData(netnwd, park = park, sitecode = site,
+#                           charname = char, years = 2019) %>% 
+#                  mutate(month = lubridate::month(Date, label = TRUE, abbr = FALSE),
+#                         mon_num = as.numeric(month))
+# water_dat_new$year <- format(as.Date(water_dat_new$Date), "%Y")
 
 #----- Boxplot -----
 site_plot <- 
-  ggplot(data = water_dat_hist, aes(x = site, y = ValueCen) +
-  geom_boxplot()
+  ggplot(data = water_dat_hist, aes(x = Site, y = ValueCen, fill = Site)) +
+  geom_boxplot() +
+  geom_jitter() +
+  forestMIDN::theme_FVM()
   
-
 site_plot
 
 
