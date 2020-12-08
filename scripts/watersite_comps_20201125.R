a2ebaddb768e05420488644d05cb181805224e7f
ldgurvich <ldgurvich@gmail.com>
2020-12-08 11:48:26 -0500

    added active sites check

--- scripts/watersite_comps_20201125.R
+++ scripts/watersite_comps_20201125.R
@@ -80,8 +80,11 @@ setMethod(f = "watersite_comps", signature = c(object = "NCRNWaterObj"),
                                     year = lubridate::year(Date)) %>% 
               arrange(Date)
             
-            site.key <- data.frame(Site =  getSiteInfo(netnwd, parkcode = parkcode, info = "SiteCode"),
-                                   SiteName =  getSiteInfo(netnwd, parkcode = parkcode, info = "SiteName"))
+            active_sites <- wdat %>% select(Site) %>% unique()
+            
+            site.key <- data.frame(Site =  getSiteInfo(object, parkcode = parkcode, info = "SiteCode"),
+                                   SiteName =  getSiteInfo(object, parkcode = parkcode, info = "SiteName")) %>% 
+              filter(Site %in% active_sites$Site)
             
             if(nrow(wdat) == 0){
               stop(paste0("There are no data available to plot for year: ", year, "."))
