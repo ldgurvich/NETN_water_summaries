db10f0b0020fe22deb2e2f0301f800c47d806835
ldgurvich <ldgurvich@gmail.com>
2022-09-14 16:24:42 -0400

    edit output file name

--- scripts/generate_briefs_all_parks.R
+++ scripts/generate_briefs_all_parks.R
@@ -8,12 +8,12 @@ library(tidyverse)
 lnetn_parks <- c("MABI", "MIMA", "MORR", "ROVA", "SAGA", "SARA", "WEFA")
 indir <- paste0("./rmd/")
 outdir <- paste0("./rmd/output/")
-rmd <- c("NETN_automated_summary_physical_params_20201124.Rmd")
+rmd <- c("LNETN_automated_summary_20210219.Rmd")
 
 render_reports <- function(park_list){
   rmarkdown::render(input = paste0(indir, rmd),
                     params = list(park = park_list),
-                    output_file = paste0("Lower_NETN_WQ_physical_parameters_", park_list, ".html"),
+                    output_file = paste0("Lower_NETN_WQ_parameters_", park_list, ".html"),
                     output_dir = outdir)
   
 }
